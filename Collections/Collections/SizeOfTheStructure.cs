f738693844bc0f6617db4744d56cef5e49f1fbf0
Oleg Turelskiy <o.turelskiy@mail.ru>
2023-04-29 14:54:09 +0300

    Get size of the sctructure

---
+++ Collections/Collections/SizeOfTheStructure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class SizeOfTheStructure
+    {
+        public static int GetSize<T>()
+        {
+            Type tt = typeof(T);
+            int size;
+            if (tt.IsValueType)
+            {
+                if (tt.IsGenericType)
+                {
+                    var t = default(T);
+                    size = Marshal.SizeOf(t);
+                }
+                else
+                {
+                    size = Marshal.SizeOf(tt);
+                }
+            }
+            else
+            {
+                size = IntPtr.Size;
+            }
+            return size;
+        }        
+    }
+}
