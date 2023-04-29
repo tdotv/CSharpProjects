04ae1a519cb9b8f986928cd9ef0e17ed0df07611
Oleg Turelskiy <o.turelskiy@mail.ru>
2023-04-29 10:00:40 +0300

    O(1)

---
+++ GrokkingAlgorithms/hash_table_price/hash_table_price/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var book = new Dictionary<string, decimal>
+            {
+                { "apple", 0.67m },
+                { "milk", 1.49m },
+                { "avocado", 1.49m }
+            };
+
+            foreach (var item in book)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
