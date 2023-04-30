ffda7dcd6159e13a7fe0a974066cbbbd093d3a3d
Oleg Turelskiy <o.turelskiy@mail.ru>
2023-04-30 23:06:02 +0300

    Amazing algorithm with comment in the below

--- GrokkingAlgorithms/hash_table_price/hash_table_price/Program.cs
+++ GrokkingAlgorithms/hash_table_price/hash_table_price/Program.cs
@@ -19,5 +19,26 @@ namespace ConsoleApplication
                 Console.WriteLine($"{item.Key}: {item.Value}");
             }
         }
+
+        public class Solution   // Solution from LeetCode
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                Dictionary<int, int> map = new Dictionary<int, int>();
+                int[] result = new int[2];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int complement = target - nums[i];
+                    if (map.ContainsKey(complement))
+                    {
+                        result[0] = map[complement];
+                        result[1] = i;
+                        return result;
+                    }
+                    map[nums[i]] = i;
+                }
+                return result;
+            }
+        }
     }
 }
