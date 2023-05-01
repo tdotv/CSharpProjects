2aba5ce48677ac3ed65136c75adff7e97b336074
Oleg Turelskiy <o.turelskiy@mail.ru>
2023-05-01 15:02:12 +0300

    Palindrome is added

--- LeetCode/Solution/Solution/Program.cs
+++ LeetCode/Solution/Solution/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpTest
+﻿using System.Linq;
+
+namespace CSharpTest
 {
     public class Program
     {
@@ -14,19 +16,15 @@
                 this.next = next;
             }
         }
-
-        //  Definition for singly-linked list.
-        //  Input: l1 = [2,4,3], l2 = [5,6,4]
-        //  Output: [7,0,8]
-        //  Explanation: 342 + 465 = 807.
-        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)                          
+       
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)  //  Input: l1 = [2,4,3], l2 = [5,6,4]
         {
-            ListNode dummyHead = new ListNode(0); // head of newly generated list
+            ListNode dummyHead = new ListNode(0);                       // head of newly generated list
             ListNode curr = dummyHead;
-            int sum = 0;
-            int carry = 0;
+            int sum = 0;                                                //  Output: [7,0,8]
+            int carry = 0;                                              //  Explanation: 342 + 465 = 807.
 
-            while (l1 != null || l2 != null || carry != 0) //carry != 0 for the last node
+            while (l1 != null || l2 != null || carry != 0)              //  carry != 0 for the last node
             {
                 sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
                 curr.next = new ListNode(sum % 10);
@@ -42,9 +40,23 @@
             return dummyHead.next;
         }
 
+        //  2
+
+        public static bool IsPalindrome(int x)
+        {
+            int r = 0, c = x;   // r = 0, c = x = 121
+            while (c > 0)
+            {
+                r = r * 10 + c % 10;    //  r = 0 + 1 -> r = 12 -> r = 121
+                c /= 10;                //  c = 12    -> c = 1  -> c = 0
+            }
+            return r == x;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hihi");
+            int x = 121;
+            Console.WriteLine(IsPalindrome(x));
         }
     }
 }
