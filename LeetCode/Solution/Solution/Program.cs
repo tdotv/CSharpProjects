0bf720495f7eff365a91e3def48effea7aac7ae0
Oleg Turelskiy <o.turelskiy@mail.ru>
2023-05-08 20:58:01 +0300

    upd

--- LeetCode/Solution/Solution/Program.cs
+++ LeetCode/Solution/Solution/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
 
 namespace CSharpTest
 {
     public class Program
     {
-        //  1
+        //  2
         public class ListNode
         {
             public int val;
@@ -40,8 +42,7 @@ namespace CSharpTest
             return dummyHead.next;
         }
 
-        //  2
-
+        //  9
         public static bool IsPalindrome(int x)
         {
             int r = 0, c = x;   // r = 0, c = x = 121
@@ -53,8 +54,7 @@ namespace CSharpTest
             return r == x;
         }
 
-        //  3
-
+        //  35
         public static int SearchInsert(int[] nums, int target)      //  Input: nums = [1,3,5,6], target = 2
         {                                                           //  Output: 1
             var low = 0;
@@ -86,16 +86,126 @@ namespace CSharpTest
             return low;
         }
 
-        public static int SearchInsert2(int[] nums, int target)
+        //  14
+        public static string LongestCommonPrefix(string[] strs)
         {
-            for (int i = 0; i < nums.Length; i++)
+            if (strs.Length == 0 || Array.IndexOf(strs, "") != -1)
+                return "";
+            string result = strs[0];
+            int i = result.Length;
+            foreach (string word in strs)
             {
-                if (target == nums[i] || nums[i] > target)
+                int j = 0;
+                foreach (char c in word)
                 {
-                    return i;
+                    if (j >= i || result[j] != c)
+                        break;
+                    j += 1;
+                }
+                i = Math.Min(i, j);
+            }
+            return result.Substring(0, i);
+        }
+
+        //  3
+        public static int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, int> set = new Dictionary<char, int>();
+            int maxSub = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (!set.ContainsKey(c))
+                {
+                    set.Add(c, i);
+                    maxSub = Math.Max(set.Count, maxSub);
+                }
+                else
+                {
+                    i = set[c];
+                    set.Clear();
                 }
             }
-            return nums.Length;
+            return maxSub;
+        }
+
+        //  21
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)    // Input: list1 = [1,2,4], list2 = [1,3,4]  Output: [1,1,2,3,4,4]
+        {
+            if (list1 == null && list2 == null ) return list2;
+            ListNode current = new(0);
+            ListNode head = current;
+            while (list1 != null && list2 != null)                              // Input: list1 = [], list2 = []            Output: []
+            {
+                if (list1.val < list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+            if (list1 != null)
+            {
+                current.next = list1;
+            }
+            if (list2 != null)
+            {
+                current.next = list2;
+            }
+            return head.next;
+        }
+
+        //  7
+        public static int Reverse(int x)
+        {
+            int res = 0;
+            while (x != 0)
+            {
+                int tmp = x % 10;
+                res = res * 10 + tmp;
+                x = (x - tmp) / 10;
+
+                if (res % 10 != tmp) // !exceed int range!  1534236469
+                {
+                    return 0;
+                }
+            }
+            return res;
+        }
+
+        //  62  Dynamic programming
+        public static int UniquePaths(int m, int n)
+        {
+            if (m == 0 && n == 0)
+            {
+                return 0;
+            }
+            int[][] arr = new int[m][];     //  two-dimensional array init
+            for (int i = 0; i < m; i++)
+            {
+                arr[i] = new int[n];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i==0 && j==0) || (i!=0 && j==0) || (i==0 && j!=0))
+                    {
+                        arr[i][j] = 1;
+                    }
+                    else
+                    {
+                        arr[i][j] = arr[i-1][j] + arr[i][j-1];
+                    }
+                }
+            }
+            return arr[m-1][n-1];
         }
 
         static void Main(string[] args)
