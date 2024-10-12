8515b55efaaf391f465051cd8f3abb4a2bfb644b
Oleg Turelskiy <o.turelskiy@mail.ru>
2024-10-12 18:24:08 +0300

    LINQ learning

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -34,7 +34,22 @@ namespace LINQ
             // System.Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(";").Contains("abb"));
 
             // 7. Get the longest word in string "aaa;xabbx;abb;ccc;dap"
-            System.Console.WriteLine("aaa;xabbx;abb;ccc;dap");
+            // Console.WriteLine(string.Join(" ", "aaa;xabbx;abb;ccc;dap".Split(';').OrderByDescending(w => w.Length).First()))
+            // Console.WriteLine(string.Join(" ", "aaa;xabbx;abb;ccc;dap".Split(';').Aggregate((longest, next) => next.Length > longest.Length ? next : longest)));
+
+            // 8. Calculate average length of word in string "aaa;xabbx;abb;ccc;dap"
+            // Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Average(w => w.Length));
+            // Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Select(word => word.Length).Average());
+
+            // 9. Print the shortest word reversed in string "aaa;xabbx;abb;ccc;dap;zh"
+            // Console.WriteLine(new string("aaa;xabbx;abb;ccc;dap;zh".Split(';').OrderBy(word => word.Length).First().Reverse().ToArray()));
+            // Console.WriteLine("aaa;xabbx;abb;ccc;dap;zh".Split(';').Aggregate((shortest, next) => next.Length < shortest.Length ? next : shortest).Reverse().ToArray());
+
+            // ??? 10. Print true if in the first word that starts from "aa" all letters are 'b' otherwise false "baaa;aabb;aaa;xabbx;abb;ccc;dap;zh"
+            // Console.WriteLine("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').FirstOrDefault(w => w.StartsWith("aa"))?.All(c => c == 'b'));
+
+            // 11. Print last word in sequence that excepting first two elements that ends with "bb"
+            // Console.WriteLine("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').Skip(2).LastOrDefault(word => word.EndsWith("bb")));
         }
     }
 }
